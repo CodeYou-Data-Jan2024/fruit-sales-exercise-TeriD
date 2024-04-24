517f42adee88b0c2de9b61b8dedc7c4a9c0b301d
Teri Dowdy <Teri.Dowdy@gmail.com>
2024-04-24 15:47:33 -0400

    Changes to be committed: 	modified:   fruit.csv 	modified:   fruitsales.py

--- fruitsales.py
+++ fruitsales.py
@@ -7,13 +7,14 @@ py_file_path = os.path.dirname(os.path.abspath(__file__))
 print(py_file_path)
 
 # Data for the CSV file
-data = {'Apples': [35, 41], 'Bananas': [21, 34],}
+data = {'Apples': [35, 41], 'Bananas': [21, 34], }
 labels = ['2017 Sales', '2018 Sales']
 
 # Create the DataFrame
 fruit_sales = pd.DataFrame(data, index=labels)
 
-print(fruit_sales) # test output
+# Print the DataFrame to test output
+print(fruit_sales)
 
 # Define CSV file path
 csv_file_path = os.path.join(py_file_path, 'fruit.csv')
